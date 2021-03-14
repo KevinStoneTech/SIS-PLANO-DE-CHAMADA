fe8d3906e344b2fbb251331883fe3ea5e258b2e5
Kevin Stone <kevin.stone.silva@gmail.com>
2021-03-14 08:53:08 -0400

    Criação do delete da subunidade

---
+++ subunid_delete.php
@@ -0,0 +1,24 @@
+<?php
+
+include("conexao.php");
+$pdo_u = conectar("membros");
+$tabela = "subunidade";
+
+
+
+
+//$id = $_GET["id"];
+
+$idusuario = base64_decode(filter_input(INPUT_GET, "tkusr"));
+$deletar = $pdo_u->prepare("DELETE FROM $tabela WHERE id = '$id'");
+$deletar->execute();
+
+
+
+if($deletar):
+        echo "<script> alert('Deletado com sucesso!'); location.href='cad_usu_comp.php'; </script>";
+             //header("Location: cad_usu_comp.php");
+endif;
+
+
+?>
