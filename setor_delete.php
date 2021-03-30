06b80b36264b546201d835903c3afb59ac39b8e9
Kevin Stone <kevin.stone.silva@gmail.com>
2021-03-30 15:39:37 -0400

    Inclusão do excluir setores

--- setor_delete.php
+++ setor_delete.php
@@ -6,7 +6,6 @@ $tabela = "setores";
 
 $id = $_GET["id"];
 
-$idsetor = base64_decode(filter_input(INPUT_GET, "tkusr"));
 $deletar = $pdo_u->prepare("DELETE FROM $tabela WHERE id = '$id'");
 $deletar->execute();
 
