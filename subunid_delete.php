8a203b9311c5977caa08933234db254eef84c965
Kevin Stone <kevin.stone.silva@gmail.com>
2021-03-30 15:41:20 -0400

    Inclusão do excluir setores e subunidades

--- subunid_delete.php
+++ subunid_delete.php
@@ -6,14 +6,12 @@ $tabela = "subunidade";
 
 $id = $_GET["id"];
 
-$idsub = base64_decode(filter_input(INPUT_GET, "tkusr"));
 $deletar = $pdo_u->prepare("DELETE FROM $tabela WHERE id = '$id'");
 $deletar->execute();
 
 
 if($deletar):
         echo "<script> alert('Deletado com sucesso!'); location.href='cad_subunid.php'; </script>";
-             //header("Location: cad_usu_comp.php");
 endif;
 
 
